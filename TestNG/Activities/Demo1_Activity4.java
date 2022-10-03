1fdbc9ea8ce9893dc2ea0dd3ba3da77f3b99b507
Aishwarya-ravi3 <103920975+Aishwarya-ravi3@users.noreply.github.com>
2022-10-03 12:05:57 +0530

    Add files via upload

---
+++ TestNG/Activities/Demo1_Activity4.java
@@ -0,0 +1,16 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class Demo1_Activity4 {
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in first test case from demoOne Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in second test case from demoOne Class");
+    }
+}
+
---
+++ TestNG/Activities/Demo2_Activity4.java
@@ -0,0 +1,10 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class Demo2_Activity4 {
+	@Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
